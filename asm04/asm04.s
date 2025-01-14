485343677145f2cf14f31450861bbbe2995ffbc0
Fbetterki <fahim77.ff@gmail.com>
2025-01-14 08:33:28 +0100

    asm04 original

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -14,44 +14,25 @@ _start:
     syscall
 
     mov rdi, input
-    xor rcx, rcx
-
-verifier_input:
-    mov al, byte [rdi + rcx]
-    cmp al, 0
-    je dernier
-    cmp al, 0x30
-    jb bad_input
-    cmp al, 0x39
-    ja bad_input
-    inc rcx
-    jmp verifier_input
-
-bad_input:
-    mov rax, 60
-    mov rdi, 2
-    syscall
-
-dernier:
-    mov rdi, input
     add rdi, 10
 
-retour_dernier:
+dernier:
     cmp byte [rdi], 0x30
     jb verifier
     cmp byte [rdi], 0x39
     ja verifier
-    jmp resultat
+    jmp resultat            
 
 verifier:
-    dec rdi
-    jmp retour_dernier
+    dec rdi                       
+    jmp dernier
 
 resultat:
-    movzx eax, byte [rdi]
-    sub eax, '0'
-    and eax, 1
-    jnz impair
+    movzx eax, byte [rdi]         
+    sub eax, '0'                  
+
+    and eax, 1                    
+    jnz impair                      
 
     mov rax, 60
     mov rdi, 0
